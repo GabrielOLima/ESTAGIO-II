c821e5e8580b941b603979890431d048b7cde406
auri <auritp19@gmail.com>
2018-10-03 22:15:00 -0300

    alterando senha

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView, UpdateView
+from django.views.generic import  (
+    CreateView, TemplateView, UpdateView, FormView
+)
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin #função quando sua view é baseada em classe
+from django.contrib.auth.forms import PasswordChangeForm
 
 from .models import User
 from .forms import UserAdminCreationForm
@@ -18,10 +21,10 @@ class RegisterView(CreateView):
     form_class = UserAdminCreationForm
     success_url = reverse_lazy('index')
 
-class UpdateUserView(LoginRequiredMixin, UpdateView):
+class UpdateUserView(LoginRequiredMixin, UpdateView): # O LoginRequiredMixin é para ver se o úsuario nao é nulo
 
     model = User
-    template_name = 'accounts/update_user.html'
+    template_name = 'accounts/update_user.html' #templates a serem criados na pasta accounts
     fields = ['name', 'email']
     success_url = reverse_lazy('accounts:index')
 
@@ -29,8 +32,21 @@ class UpdateUserView(LoginRequiredMixin, UpdateView):
         return self.request.user
 
 
+class UpdatePasswordView(LoginRequiredMixin, FormView):
+
+    template_name = 'accounts/update_password.html'
+    success_url = reverse_lazy('accounts:index')
+    form_class = PasswordChangeForm
+
+    def get_form_kwargs(self):
+        kwargs = super(UpdatePasswordView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
 
 index = IndexView.as_view()
 register = RegisterView.as_view()
-update_user = UpdateView.as_view()
+update_user = UpdateUserView.as_view()
+update_password = UpdatePasswordView.as_view()
+
 # Create your views here.
